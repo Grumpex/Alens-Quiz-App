d4c8d439465bf818094f3d13d99f1a0c1b1cfa85
Grumpex <gamingwithalen@gmail.com>
2024-09-01 21:37:09 +0200

    Added loader and API for random questions | Project finished

--- end.js
+++ end.js
@@ -2,7 +2,7 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
@@ -15,11 +15,11 @@ function saveHighScore(event) {
     event.preventDefault()
 
     const score = {
-        score: Math.floor(Math.random() * 100),
+        score: mostRecentScore,
         name: username.value
     };
     highScores.push(score);
-    highScores.sort( (a,b) => b.score - a.score);
+    highScores.sort((a,b) => b.score - a.score);
     highScores.splice(5);
     localStorage.setItem('highScores',JSON.stringify(highScores));
     window.location.assign('/');
