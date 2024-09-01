62b7411282b3afb0a5902d9dab40e3bfdca5d010
Grumpex <gamingwithalen@gmail.com>
2024-09-01 20:33:54 +0200

    Save highscores in LS

--- end.js
+++ end.js
@@ -2,15 +2,25 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', function () {
-    console.log(username.value)
     saveScoreBtn.disabled = !username.value;
 });
 
 function saveHighScore(event) {
-    console.log("clicked the save button");
     event.preventDefault()
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    window.location.assign('/');
 }
